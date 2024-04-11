1e2b7c955f823027e00af4e3105c5fb1b86d203e
krishiraj123 <krishirajvansia123@gmail.com>
2024-04-11 20:48:54 +0530

    updating title in main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: "Exam",
+      title: "CRUD DEMO",
       debugShowCheckedModeBanner: false,
       home: DisplayAPIPage(),
     );
